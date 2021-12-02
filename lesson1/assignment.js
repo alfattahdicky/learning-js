3270e8acdd8a4b138eced610c23f6c85d4c79d60
alfattahdicky <alfattahdicky2@gmail.com>
2021-12-03 05:36:13 +0700

    Learn JS : Assignment JS

---
+++ lesson1/assignment.js
@@ -0,0 +1,28 @@
+/**
+   JavaScript Assignment
+   	 Assignment operators assign values to JavaScript Variables
+   	 	- adds value 
+   	 	- substracts value
+   	 	- multiplies value
+   	 	- divides values
+   	 	- remainder values
+*/
+let x = 0;
+
+// Adds Value
+x += 5; // 5
+
+// Substract value
+x -= 3; // 2
+
+// multiplies value
+x *= 5; // 10
+
+// divides value
+x /= 5; // 2
+
+// remainder value
+x %= 3; // 1
+
+
+
